2ad28966a1eacad884f5ee5800900284d3ad8e46
Ruan <ruanhasselmanndasilva@gmail.com>
2021-05-20 23:52:01 -0300

    Update layouts

--- GestanteDoZap/app/src/main/java/com/example/gestantedozap/MainActivity.java
+++ GestanteDoZap/app/src/main/java/com/example/gestantedozap/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.gestantedozap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        menu = findViewById(R.id.menu);
+    }
+
+    public void menu(View view) {
+        if(menu.getVisibility()==View.GONE){
+            menu.setVisibility(View.VISIBLE);
+        }else{
+            menu.setVisibility(View.GONE);
+        }
     }
 }
