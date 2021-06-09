eaecd365590e9bfee2e05d1fb5c3ff46b3a94dfa
Ruan <ruanhasselmanndasilva@gmail.com>
2021-06-08 21:22:58 -0300

    Ajustes spinner tela de cadastro;

--- GestanteDoZap/app/src/main/java/com/example/gestantedozap/Cadastro.java
+++ GestanteDoZap/app/src/main/java/com/example/gestantedozap/Cadastro.java
@@ -2,7 +2,10 @@ package com.example.gestantedozap;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,11 +13,16 @@ import android.os.Bundle;
 public class Cadastro extends AppCompatActivity {
 
     LinearLayout parto;
+    Spinner conheceu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
         parto = findViewById(R.id.parto);
+        conheceu = (Spinner) findViewById(R.id.conheceu);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.conheceu_app, android.R.layout.simple_spinner_dropdown_item);
+        conheceu.setAdapter(adapter);
     }
 
     public void sim(View view) {
@@ -26,6 +34,10 @@ public class Cadastro extends AppCompatActivity {
     }
 
     public void cadastrar(View view) {
+        //Metodo para pegar o conteudo do spiner de como conhecer o app, para adicionar mais opções mudar no arquivo strings.xml
+        /*String conhecimento = conheceu.getSelectedItem().toString();
+        Toast.makeText(getApplicationContext(), "Metodo escolhido: " + conhecimento, Toast.LENGTH_SHORT).show();*/
+
         Intent it = new Intent(this, MainActivity.class);
         startActivity(it);
         finish();
