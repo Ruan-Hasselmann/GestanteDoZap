b37505776f6a9b2c09dc419eb828a727584afa2c
Ruan <ruanhasselmanndasilva@gmail.com>
2021-07-05 21:48:23 -0300

    Requisições get api;

--- GestanteDoZap/app/src/main/java/com/example/gestantedozap/MainActivity.java
+++ GestanteDoZap/app/src/main/java/com/example/gestantedozap/MainActivity.java
@@ -1,24 +1,79 @@
 package com.example.gestantedozap;
 
 import android.net.Uri;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import com.android.volley.*;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout menu;
+    TextView textView;
+    RequestQueue queue = null;
+
+    public static final String TAG = "GET";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         menu = findViewById(R.id.menu);
+        textView = findViewById(R.id.textView);
 
-        
+    }
+
+    private void exibirPostagens() {
+
+        queue = Volley.newRequestQueue(this);
+        String url = "http://191.233.255.192/api/postagem";
+
+        JsonArrayRequest JsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Toast.makeText(MainActivity.this, "Resposta", Toast.LENGTH_SHORT).show();
+                        try {
+                            Toast.makeText(MainActivity.this, ""+response.length(), Toast.LENGTH_SHORT).show();
+                            for (int i = 0; i <= response.length(); i++){
+                                textView.setText("Response: " + response.get(i));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Toast.makeText(MainActivity.this, "Erro na solicitação" + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        textView.setText(error.toString());
+                    }
+                });
+
+        JsonArrayRequest.setTag(TAG);
+        RetryPolicy policy = new DefaultRetryPolicy(10000, 1, 2);
+        JsonArrayRequest.setRetryPolicy(policy);
+        Log.d("Request", JsonArrayRequest.toString());
+        queue.add(JsonArrayRequest);
+
+        // Access the RequestQueue through your singleton class.
+        //MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
     }
 
     public void menu(View view) {
@@ -50,5 +105,9 @@ public class MainActivity extends AppCompatActivity {
     public void abrirInsta(View view) {
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/gestantedozap/"));
         startActivity(browserIntent);
+    }
+
+    public void teste(View view) {
+        exibirPostagens();
     }
 }
